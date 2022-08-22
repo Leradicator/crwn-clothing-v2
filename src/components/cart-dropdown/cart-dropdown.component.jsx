fb44ecdfdfed81295ef807dc9a5fdcd281dfc393
Issam O <issamoutassourt@gmail.com>
2022-08-22 06:04:30 +0100

    added cart functionality

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
+import Button from '../button/button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+    const { cartToggle } = useContext(
+        CartContext
+    );
+
+    return (
+        <div className='cart-dropdown-container' >
+            <div className='cart-items'> 
+            </div>
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    )
+ }
+
+export default CartDropdown;
