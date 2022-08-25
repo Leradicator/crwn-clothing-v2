085d214b85bf55cdc83e253ad6769537eea62a53
Issam O <issamoutassourt@gmail.com>
2022-08-25 05:46:18 +0100

    added number of objects functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,14 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-    const { toggle, setToggle } = useContext(CartContext);
+    const { toggle, setToggle, cartCount } = useContext(CartContext);
 
     const toggleDropdown = () => setToggle(!toggle);
 
     return (
         <div className='cart-icon-container' onClick={toggleDropdown}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
@@ -24,16 +24,25 @@ export const CartContext = createContext({
     setToggle: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
     const [toggle, setToggle] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => {
+            return total + cartItem.quantity;
+        }, 0)
+        setCartCount(newCartCount);
+    }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
-    const value = { toggle, setToggle, cartItems, addItemToCart, };
+    const value = { toggle, setToggle, cartItems, addItemToCart, cartCount};
     return (
         <CartContext.Provider value={value}>
             {children}
